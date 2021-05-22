b561db6ba5985b91ef5fd630f385e584c9ef83f8
Hope Crystal <hope.orobosa.henry@gmail.com>
2021-05-22 12:43:53 +0100

    Weather App v1.2

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function setQuery(evt) {
 
 function getResults(query) {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-        return weather.json();
-    }).then(displayResults);
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
 }
 
 function displayResults(weather) {
@@ -30,25 +30,45 @@ function displayResults(weather) {
     let temp = document.querySelector('.current .temp')
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>&deg;c</span>`;
 
-    let weather_el = document.querySelector('.current .weather')
+    let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
     let hiLow = document.querySelector('.hi-low')
-    hiLow.innerText =  `${Math.round(weather.main.temp_min)}oC  / ${Math.round(weather.main.temp_max)}oC`;
+    hiLow.innerHTML = `Temp Min: ${Math.round(weather.main.temp_min)} <br>
+                        Temp Max: ${Math.round(weather.main.temp_max)} <br>
+                        Humidity: ${Math.round(weather.main.humidity)} <br>
+                        Pressure: ${Math.round(weather.main.pressure)} <br>
+                        Feels Like: ${Math.round(weather.main.feels_like)}`;
+
+    function weatherImg() {
+        if (weather_el.innerText == "Rain") {
+            document.getElementById("body").style.backgroundImage = 'url("images/rain.gif")'
+        }
+        if (weather_el.innerText == "Clouds") {
+            document.getElementById("body").style.backgroundImage = 'url("images/clouds.jpg")'
+        }
+        if (weather_el.innerText == "Clear") {
+            document.getElementById("body").style.backgroundImage = 'url("images/sunny.gif")'
+        }
+
+    }
+    weatherImg()
 }
 
 function dateBuilder(d) {
-    let months =  ["January", "February", "March", "April", "May", "June", "July",
-    "July", "August", "September", "October", "November", "December"];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+        "July", "August", "September", "October", "November", "December"
+    ];
 
     let days = [
         "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday"];
+        "Friday", "Saturday"
+    ];
 
-        let day = days[d.getDay()];
-        let date = d.getDate()
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
+    let day = days[d.getDay()];
+    let date = d.getDate()
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-        return `${day} ${date} ${month} ${year}`
+    return `${day} ${date} ${month} ${year}`
 }
